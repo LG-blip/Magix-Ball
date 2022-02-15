baed9103925eafbba4f42923ad95b1d601c12cd0
Ganesh L <g021306@gmail.com>
2022-02-15 10:38:58 +0530

    randomized decision maker

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('ASK ME ANYTHING'),
+          ),
+          body: AskMeApp(),
+        ),
       ),
     );
+
+class AskMeApp extends StatefulWidget {
+  const AskMeApp({Key key}) : super(key: key);
+
+  @override
+  _AskMeAppState createState() => _AskMeAppState();
+}
+
+class _AskMeAppState extends State<AskMeApp> {
+  int ballimagenumber = 1;
+  void changeballno() {
+    ballimagenumber = Random().nextInt(5) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Expanded(
+        child: TextButton(
+          onPressed: () {
+            setState(() {
+              changeballno();
+            });
+          },
+          child: Image.asset('images/ball$ballimagenumber.png'),
+        ),
+      ),
+    ]);
+  }
+}
